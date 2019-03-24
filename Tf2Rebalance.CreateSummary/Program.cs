c3399d4bad1e2d9a861e4fd069996ade566038e9
eisbaer66 <janweidner87@gmail.com>
2019-03-24 21:24:04 +0100

    Add project files.

---
+++ Tf2Rebalance.CreateSummary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tf2Rebalance.CreateSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
